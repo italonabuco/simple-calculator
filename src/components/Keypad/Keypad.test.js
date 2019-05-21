e7413ac03db8c1fcda9e33c37fb2198325d68ae0
Italo Nabuco <italonabuco@hotmail.com>
2019-05-20 23:32:27 -0300

    Updating Keypad

--- src/components/Keypad/Keypad.test.js
+++ src/components/Keypad/Keypad.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Keypad from './Keypad';
 
 describe('Keypad', () => {
@@ -18,18 +18,44 @@ describe('Keypad', () => {
         );
     });
 
+    it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
     it('should render a <div />', () => {
-        expect(wrapper.find('div').length).toEqual(3);
+        expect(wrapper.find('div').length).toEqual(4);
     });
 
-    it('renders the values of numbers', () => {
+    it('should render an instance of the Key component for each index of numbers, operators, and the submit Key', () => {
+        const numbers = ['0', '1', '2'];
+        const operators = ['+', '-', '*', '/'];
+        const submit = 1;
+        const keyTotal = numbers.length + operators.length + submit;
+        wrapper.setProps({ numbers, operators });
+        expect(wrapper.find('Key').length).toEqual(keyTotal);
+    });
+});
+
+describe('mounted Keypad', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(
+            <Keypad 
+                // jest.fn() creates a mock function
+                callOperator={jest.fn()}
+                numbers={[]}
+                operators={[]}
+                setOperator={jest.fn()}
+                updateDisplay={jest.fn()}
+            />
+        );
+    });
+
+    it('renders the values of numbers to the DOM', () => {
         wrapper.setProps({ numbers: ['0', '1', '2'] });
         expect(wrapper.find('.numbers-container').text()).toEqual('012');
     });
 
-    it('renders the values of operators', () => {
+    it('renders the values of operators to the DOM', () => {
         wrapper.setProps({ operators: ['+', '-', '*', '/']});
         expect(wrapper.find('.operators-container').text()).toEqual('+-*/')
     });
-
-});
+})
